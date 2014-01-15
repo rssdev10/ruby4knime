4da5191162b09f89a2c85891a0ec90981fa8f9e4
rssdev10 <rssdev10@gmail.com>
2014-01-15 22:19:13 +0400

    added script node with 2 inputs and 2 outputs

---
+++ RubyScript/src/org/knime/ext/jruby/RubyScriptNodeFactory22.java
@@ -0,0 +1,19 @@
+package org.knime.ext.jruby;
+
+/**
+ * <code>NodeFactory</code> for the "RubyScript" Node.
+ * 
+ *
+ * @author 
+ */
+public class RubyScriptNodeFactory22
+        extends RubyScriptNodeFactory {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public RubyScriptNodeModel createNodeModel() {
+        return new RubyScriptNodeModel(2,2);
+    }
+}
